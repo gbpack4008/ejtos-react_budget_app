44cdc9e90aa406fb5668add514202e481102d6de
gbpack4008 <gbpack40087@gmail.com>
2024-01-25 09:18:37 -0500

    changes made from theia lab environment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
 import { TiDelete } from 'react-icons/ti';
+import { TiPlus } from 'react-icons/ti';
+import { TiMinus } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -24,15 +26,28 @@ const ExpenseItem = (props) => {
         });
 
     }
-
+    const decreaseAllocation = (name) => {
+        // alert()
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
+    
+    
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
-        </tr>
-    );
-};
-
-export default ExpenseItem;
+		<td>{props.name}</td>
+		<td>{currency}{props.cost}</td>
+		<td align="center"><TiPlus size='1.5em' onClick={event=> increaseAllocation(props.name)}style={{  backgroundColor:'#50a95b', color:'white'}}></TiPlus></td>
+		<td align="center"><TiMinus size='1.5em' onClick={event=> decreaseAllocation(props.name)}style={{ backgroundColor:'#ab2514', color:'white'}}></TiMinus></td>
+		<td align="center"><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+		</tr>
+        );
+    };
+    
+    export default ExpenseItem;
